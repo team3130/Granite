83d641027719785de9370b489a6f003386fe6d3a
Mikhail Kyraha <mikhail@kyraha.com>
2016-01-31 21:49:53 -0600

    Arduino communications
    USB connection works but hangs the whole system for 5 seconds per
    "Write". RS-232 is not communicating in this  commit yet.

--- Granite/src/Commands/RobotSensors.cpp
+++ Granite/src/Commands/RobotSensors.cpp
@@ -1,9 +1,10 @@
 #include "RobotSensors.h"
 #include "Subsystems/Chassis.h"
+#include "Video.h"
 
 RobotSensors::RobotSensors()
 {
-	arduino = new SerialPort(9600, SerialPort::kUSB);
+	arduino = new SerialPort(9600 /*, SerialPort::kUSB*/);
 	timer = new Timer();
 	this->SetRunWhenDisabled(true);
 }
@@ -25,26 +26,48 @@ void RobotSensors::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void RobotSensors::Execute()
 {
-		if(timer->Get() > 1.0) {
-			timer->Reset();
-			timer->Start();
+	if(timer->Get() > 1.0) {
+		timer->Reset();
+		timer->Start();
 /*
-			if( chassis->m_cEncoderL->GetRate() > 6 ) arduino->Write("3", 1);
-			else if( chassis->m_cEncoderL->GetRate() < -6 ) arduino->Write("4", 1);
-			else arduino->Write("5", 1);
+		if( chassis->m_cEncoderL->GetRate() > 6 ) arduino->Write("3", 1);
+		else if( chassis->m_cEncoderL->GetRate() < -6 ) arduino->Write("4", 1);
+		else arduino->Write("5", 1);
 
-			if( lifter->GetSpeed() > 0.5 ) arduino->Write("6", 1);
-			else if(lifter->GetSpeed() < -0.5) arduino->Write("7", 1);
-			else arduino->Write("8", 1);
+		if( lifter->GetSpeed() > 0.5 ) arduino->Write("6", 1);
+		else if(lifter->GetSpeed() < -0.5) arduino->Write("7", 1);
+		else arduino->Write("8", 1);
 
-			if( CommandBase::pusher->GetDir() > 0) arduino->Write("9", 1);
-			else if( CommandBase::pusher->GetDir() < 0) arduino->Write("10", 1);
-			else arduino->Write("11", 1);
+		if( CommandBase::pusher->GetDir() > 0) arduino->Write("9", 1);
+		else if( CommandBase::pusher->GetDir() < 0) arduino->Write("10", 1);
+		else arduino->Write("11", 1);
 
-			if(!lifterZero) {
-				arduino->Write("Z", 1);
-			} */
+		if(!lifterZero) {
+			arduino->Write("Z", 1);
+		} */
+	}
+
+	timer->Reset();
+	timer->Start();
+	size_t nTurns = 0;
+	double turn = 0;
+	RobotVideo::GetInstance()->mutex_lock();
+	nTurns = RobotVideo::GetInstance()->HaveHeading();
+	if(nTurns > 0) turn = RobotVideo::GetInstance()->GetTurn(0);
+	RobotVideo::GetInstance()->mutex_unlock();
+	if (nTurns>0) {
+		if (fabs(turn)<2.0) {
+			double a = timer->Get();
+			uint32_t res = arduino->Write("Solid green\n", 12);
+			std::ostringstream oss2;
+			oss2 << "ON:" << res << " X:" << a << " T:" << timer->Get();
+			SmartDashboard::PutString("DB/String 2", oss2.str());
 		}
+		else {
+			SmartDashboard::PutString("DB/String 2", "off target");
+		}
+	}
+
 	std::ostringstream oss0;
 	oss0 << "Angle: " << ChassisSubsystem::GetInstance()->GetAngle();
 	SmartDashboard::PutString("DB/String 0", oss0.str());
